870d761b72d3cf056d08ff110d73700d95180244
Sharif Jubayer Arefin <contact@jubayerarefin.info>
2022-06-21 20:42:35 +0600

    Adds required relations in models Adds a migration for the pivot table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
@@ -29,4 +30,9 @@ class Product extends Model
     protected $casts = [
         'status' => 'boolean',
     ];
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'user_product');
+    }
 }
